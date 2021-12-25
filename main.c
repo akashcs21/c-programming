e31283c14065314f6007ed99910f980c71a51c90
akashcs21 <95097465+akashcs21@users.noreply.github.com>
2021-12-25 23:28:15 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+    int year,i;
+    printf("enter the year limit to check leap year");
+    scanf("%d",&year);
+    for(i=1;i<=year;i++)
+    {
+        if(i%400==0||(i%4==0&&100!=0))
+        
+            printf("%d\n",i);
+        }
+
+return 0;
+}
+
