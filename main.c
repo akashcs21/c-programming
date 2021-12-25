353f063585c3c2bd29845f548e20abbd43c68eeb
akashcs21 <95097465+akashcs21@users.noreply.github.com>
2021-12-25 23:08:00 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("enter the constant value");
+    scanf("%c",&a);
+    printf("the size of constant value is %lu bytes",sizeof(a));
+    
+}
