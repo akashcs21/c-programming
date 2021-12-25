781600510cfbe03723788647c0f99df389ed27df
akashcs21 <95097465+akashcs21@users.noreply.github.com>
2021-12-25 23:40:00 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+  char alphabet;
+  printf("the upper case alphabet are:");
+  alphabet=65;
+  while(alphabet<=90)
+  {
+      printf("%c ",alphabet);
+      alphabet++;
+  } 
+  printf("\n");
+  printf("the lowercase alphabet are :");
+  alphabet=97;
+  while(alphabet<=122)
+  {
+      printf("%c ",alphabet);
+      alphabet++;
+  }
+ 
+   
+   
+   
+}
