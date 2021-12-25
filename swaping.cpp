56ef21ebeb2c331204d9c88e3b94b57e42c036bd
akashcs21 <95097465+akashcs21@users.noreply.github.com>
2021-12-25 23:58:50 +0530

    Add files via upload

---
+++ swaping.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int max,maxi,mini,n,i,min;
+	printf("enter the size of erray:");
+	scanf("%d",&n);
+	int a[n];
+	printf("enter the element of array:");
+	for(i=0;i<n;i++)
+	
+	{
+		scanf("%d",a[i]);
+	}
+	max=min=a[0];
+	mini=maxi=0;
+	for(i=0;i<n;i++)
+	{
+		if(max<a[i])
+		{
+			max=a[i];
+			maxi=i;
+		}
+		else if(min>a[i])
+		{
+			min=a[i];
+			mini=i;
+		}
+	} 
+	a[maxi]=min;
+	a[mini]=max;
+	printf("updated array %d",a[i]);
+	
+	
+	
+	
+	
+	
+}
