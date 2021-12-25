e4c21cedacd8b6ba6d3b9dbb0d1dd7686f21b8cf
akashcs21 <95097465+akashcs21@users.noreply.github.com>
2021-12-25 23:03:37 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("enter your age");
+    scanf("%d",&age);
+    if(age>18)
+    {
+        printf("you are eligible to vote");
+        
+    } 
+    else
+    {  printf("you are not elgible to vote\n");
+    age=18-age;
+      printf("years are left to vote=%d",age);
+    }
+}
