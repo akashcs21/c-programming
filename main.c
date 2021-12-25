672be06a6702b8af31fdeb11a9e08ccf4e89eb1f
akashcs21 <95097465+akashcs21@users.noreply.github.com>
2021-12-25 23:26:38 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i,numb;
+    printf("enter the number");
+    scanf("%d",&numb);
+    for(i=1;i<=10;i++)
+    {
+        printf("%d\n",(i*numb));
+    }
+}
